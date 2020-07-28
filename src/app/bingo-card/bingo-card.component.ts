0863e7fb185853b30cc1634f0741beb9edc5f7d0
Naveen Meesala <naveen.2048@outlook.com>
2020-07-28 09:14:51 -0400

    inital push

---
+++ src/app/bingo-card/bingo-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bingo',
+  templateUrl: './bingo-card.component.html',
+  styleUrls: ['./bingo-card.component.scss']
+})
+export class BingoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
